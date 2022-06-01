bd4b489e7de49b153a05b2548e379c4ad03b9b6f
SonaliThakur0104 <sonali.thakur0104@gmail.com>
2022-06-01 13:07:14 +0530

    commit message

---
+++ src/test/java/com/gitbash/PracticeGitBash.java
@@ -0,0 +1,12 @@
+package com.gitbash;
+
+import org.testng.annotations.Test;
+
+public class PracticeGitBash {
+
+	@Test
+	public void practiceGit() {
+		System.out.println("git bash created");
+	}
+
+}
