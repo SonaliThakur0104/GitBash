72eb5437b85614936f195f7fa0d144e088b74b99
SonaliThakur0104 <sonali.thakur0104@gmail.com>
2022-06-01 17:51:48 +0530

    handled conflict

--- src/test/java/com/gitbash/PracticeGitBash.java
+++ src/test/java/com/gitbash/PracticeGitBash.java
@@ -7,8 +7,11 @@ public class PracticeGitBash {
 	@Test
 	public void practiceGit() {
 		System.out.println("git bash created");
+          //updated person2
 
-           //updated person2
+                System.out.println("git bash updated");
+         //changes done===1
+
 	}
 
 }
